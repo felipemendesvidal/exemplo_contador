0857cf62c1d408182304b0abbd504c36e6ef91c3
felipe <contatofelipevidalmendes@gmail.com>
2023-01-09 17:23:33 -0300

    finalizado script

--- js/script.js
+++ js/script.js
@@ -1,23 +1,47 @@
-function verificar (){
+function contar_init() {
     
-    //pegar data atual
-    var data_atual = new Date();
-    var ano_atual = data_atual.getFullYear();
-    var form_ano = document.getElementById('txt-ano');
-    var resultado = document.getElementById('resultado');
-    if (form_ano.value.length == 0 || Number(form_ano.value ) > ano_atual){
-        alert('ano informado esta errado, verifique o campo ano e tente novamente');
+    // pegando elementos 
+    let   numero_fim  = document.getElementById('numero-fim');
+    let   numero_passo = document.getElementById('numero-passo');
+    let   txt_numero_inicio  = document.getElementById('txt-numero-inicio');
+    let   resultado = document.getElementById('resultado');
+
+
+    // verificar se os dados estão faltando
+    if (txt_numero_inicio.value.length == 0 || numero_fim.value.length == 0|| numero_passo.value.length == 0){
+        alert('erro, falta dados');
     }else{
-        var form_sex = document.getElementsByName('radsex');
-        var idade = ano_atual - Number(form_ano.value)
-        
-        var genero = '';
-        if(form_sex[0].checked){
-            genero = 'Homem';
-        }else if (form_sex[1].checked){
-            genero = 'mulher'
+
+        // adicionando no html
+        resultado.innerHTML = "contando";
+
+        let numero_contado_inicio = Number(txt_numero_inicio.value);
+        let numero_contado_passo = Number(numero_passo.value);
+        let numero_contado_fim = Number(numero_fim.value);
+        // se o passo for zero
+        if ( numero_contado_passo <= 0 ){
+            alert('passo invalido, considerando passo 1');
+            numero_contado_passo = 1
         }
-        resultado.innerHTML = `Calculamos que é um (a) ${genero} de ${idade} anos`
+        // essa contagem só funciona se o numero inicial for maior que o de baixo
+        if (numero_contado_inicio < numero_contado_fim){
+            //contagem cresente
+            for(
+                let inicio_contador_for = numero_contado_inicio; inicio_contador_for <= numero_contado_fim; inicio_contador_for += numero_contado_passo){
+                    resultado.innerHTML += `${inicio_contador_for}`;
+                }
+        }else {
+            //contagem decrecente
+            for(
+                let inicio_contador_for = numero_contado_inicio; inicio_contador_for >= numero_contado_fim; inicio_contador_for -= numero_contado_passo){
+                
+                    resultado.innerHTML += ` ${inicio_contador_for} `; 
+            }
+        }
+        
+        // formatação unicode para javascript
+        resultado.innerHTML+=`\u{1F3C1}`
+
 
     }
     
